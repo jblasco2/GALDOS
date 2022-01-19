a7f7b43a03680e1ec28d4390413141c70240fd59
Francisco Javier Blasco Pascual <84475547+jblasco2@users.noreply.github.com>
2022-01-19 18:17:41 +0100

    Add files via upload

---
+++ codigo/PCA.R
@@ -0,0 +1,18 @@
+#########################################
+## ANÁLISIS DE COMPONENTES PRINCIPALES
+##########################################
+
+# Establecer sesión
+setwd("~/Desktop/GALDOS/data/corpus")
+
+# Cargar librería 
+library(stylo)
+stylo()
+
+
+# En el menú elegir PASO 1: Texto plano -> Spanish  -> Native code
+# PASO 2 "FEATURES": Trabajo con léxico (1 palabra), Max 1000 MFW
+# PASO 3 "STATISTICS": Classic Delta, PCA 
+
+
+
